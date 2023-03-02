f77642ad49411fe9833d4ea0f9d92a0eae43a336
shoh1125 <arawaza345@gmail.com>
2023-03-02 22:07:44 +0900

    コード修正3

--- app/models/user.rb
+++ app/models/user.rb
@@ -10,9 +10,6 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   validates :password, 
              format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]+\z/}
-  validates :password_confirmation,
-             format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]+\z/}
-
   validates :last_name, 
              format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/},
              presence: true
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,7 +4,6 @@ FactoryBot.define do
     email {Faker::Internet.free_email}
     password { '1a' + Faker::Internet.password(min_length: 6) }
     password_confirmation {password}
-    encrypted_password {password}
     last_name {'太郎'}
     family_name {'田中'}
     kana_last_name {'タロウ'}
