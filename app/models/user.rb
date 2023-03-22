b3c864c3ae63d00fe1ddfa901fc763f31bb085e6
shoh1125 <arawaza345@gmail.com>
2023-03-22 15:44:39 +0900

    デプロイ後修正1

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,7 +9,7 @@ class User < ApplicationRecord
 
   validates :nickname, presence: true
   validates :password,
-            format: { with: /\A(?=.*?[a-z])(?=.*?[A-Z])(?=.*?\d)[a-zA-Z\d]+\z/ }
+            format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i}
   validates :last_name,
             format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/ },
             presence: true
